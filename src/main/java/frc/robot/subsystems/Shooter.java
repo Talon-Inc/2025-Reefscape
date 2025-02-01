669846db197dec59895237169987c8b246e13c78
Daminan Cardenas <166971132+BobbyPotato789@users.noreply.github.com>
2025-02-01 13:03:22 -0600

    Update Shooter.java

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,30 +2,55 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkPIDController;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.CANEncoder;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ShooterSubsystem extends SubsystemBase {
-  private final CANSparkMax shooterMotor;
-  private final SparkMaxPIDController pidController;
-  private final CANEncoder encoder;
-
-
 public class Shooter extends SubsystemBase {
+  private final SparkMax left_ShooterMotor = new SparkMax(0, MotorType.kBrushless);
+  private final SparkMax right_ShooterMotor = new SparkMax(0, MotorType.kBrushless);
+  private final SparkClosedLoopController left_pidController = left_ShooterMotor.getClosedLoopController();
+  private final SparkClosedLoopController right_pidController = right_ShooterMotor.getClosedLoopController();
+  private final RelativeEncoder left_encoder = left_ShooterMotor.getEncoder();
+  private final RelativeEncoder right_encoder = right_ShooterMotor.getEncoder();
+
+  public static final SparkMaxConfig left_config = new SparkMaxConfig();
+  public static final SparkMaxConfig config = new SparkMaxConfig();
   /** Creates a new Shooter. */
-  public Shooter(int motorPort) {
-    Shooter = new CANSparkMax(motorPort, MotorType.kBrushless);
-        pidController = shooter.getPIDController();
-        encoder = shooter.getEncoder(); 
- }
-}
+  public Shooter() {
+    config.idleMode(SparkBaseConfig.IdleMode.kBrake);
+
+    left_ShooterMotor.clearFaults();
+    left_ShooterMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+
+    right_ShooterMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+  }
+
+  public void shoot() {
+    left_ShooterMotor.set(0);
+    right_ShooterMotor.set(0);
+  }
+
+  public void stop() {
+    left_ShooterMotor.stopMotor();
+    right_ShooterMotor.stopMotor();
+  }
+
+
 
   @Override
   public void periodic() {
