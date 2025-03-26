df16646cf7995c53891c089dc4ce328536a2d1d1
Daminan Cardenas <166971132+BobbyPotato789@users.noreply.github.com>
2025-03-26 13:41:21 -0500

    Made Trapezoidal Profile for Arm Rotating Motor

--- src/main/java/frc/robot/subsystems/Algae.java
+++ src/main/java/frc/robot/subsystems/Algae.java
@@ -4,14 +4,82 @@
 
 package frc.robot.subsystems;
 
+import org.littletonrobotics.junction.Logger;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.AlgaeConstants;
 
 public class Algae extends SubsystemBase {
-  /** Creates a new Algae. */
-  public Algae() {}
+  /** Creates a new Algae. */ 
+  private static double kDt = 0;
+  private static double kMaxVelocity = 0;
+  private static double kMaxAccerlation = 0;
+  private static double kP = 0;
+  private static double kI = 0;
+  private static double kD = 0.0;
+  private static double kS = 0;
+  private static double kG = 0;
+  private static double kV = 0;
+  private static double ka = 0;
+  private static double lastSpeed = 0;
+  private static double lastTime = Timer.getFPGATimestamp();
+
+  private final SparkMax armMotor =
+    new SparkMax(AlgaeConstants.kLeftMotorCanId, MotorType.kBrushless);
+  
+  // Create profile constraints
+  private final TrapezoidProfile.Constraints m_constraints = new TrapezoidProfile.Constraints(kMaxVelocity, kMaxAccerlation);
+
+  // Create ProfiledPIDController Using Trapezoidal Constraints
+  private final ProfiledPIDController m_controller = new ProfiledPIDController(kP, kI, kD, m_constraints);
+
+  // Elevator FF
+  private final ArmFeedforward m_feedforward = new ArmFeedforward(kS, kG, kV, ka);
+
+  private final RelativeEncoder m_encoder = armMotor.getEncoder();
+  
+  public Algae() {
+
+    SparkMaxConfig armMotorConfig = new SparkMaxConfig();
+    armMotorConfig.smartCurrentLimit(20).voltageCompensation(12).idleMode(IdleMode.kBrake);
+
+    armMotor.configure(armMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_controller.setTolerance(0);
+  }
+
+
+  public boolean checkArmGoal() {
+    return m_controller.atGoal();
+  }
+
+  
+  public void goToPositionArm (double goalPosition) {
+    m_controller.setGoal(goalPosition);
+    double pidVal = m_controller.calculate(m_encoder.getPosition());
+    double feedForward = m_feedforward.calculate(m_controller.getSetpoint().position, m_controller.getSetpoint().velocity);
+    armMotor.setVoltage(pidVal + feedForward);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    Logger.recordOutput("Arm Encoder", m_encoder.getPosition());
+    Logger.recordOutput("Arm Velocity", m_encoder.getVelocity());
+    Logger.recordOutput("Arm Setpoint", m_controller.getSetpoint().position);
+    Logger.recordOutput("Arm Position Error", m_controller.getPositionError());
+    Logger.recordOutput("Arm Motor Current", armMotor.getOutputCurrent());
+    Logger.recordOutput("Arm Motor", m_controller.atGoal());
   }
 }
